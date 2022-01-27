4a89f192a610f70ed9ffda9960f138dde395be0c
sasakamoo <matt.sasakamoose@gmail.com>
2022-01-27 00:07:04 -0700

    cmake!

---
+++ CMakeFiles/output.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/unfal/Documents/Cool Stuff/Purple-Rain/src/GradientRain.cpp" "CMakeFiles/output.dir/src/GradientRain.cpp.obj" "gcc" "CMakeFiles/output.dir/src/GradientRain.cpp.obj.d"
+  "C:/Users/unfal/Documents/Cool Stuff/Purple-Rain/src/Poly.cpp" "CMakeFiles/output.dir/src/Poly.cpp.obj" "gcc" "CMakeFiles/output.dir/src/Poly.cpp.obj.d"
+  "C:/Users/unfal/Documents/Cool Stuff/Purple-Rain/src/app.cpp" "CMakeFiles/output.dir/src/app.cpp.obj" "gcc" "CMakeFiles/output.dir/src/app.cpp.obj.d"
+  "C:/Users/unfal/Documents/Cool Stuff/Purple-Rain/src/pge/olcPixelGameEngine.cpp" "CMakeFiles/output.dir/src/pge/olcPixelGameEngine.cpp.obj" "gcc" "CMakeFiles/output.dir/src/pge/olcPixelGameEngine.cpp.obj.d"
+  "C:/Users/unfal/Documents/Cool Stuff/Purple-Rain/src/utils/utils.cpp" "CMakeFiles/output.dir/src/utils/utils.cpp.obj" "gcc" "CMakeFiles/output.dir/src/utils/utils.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
